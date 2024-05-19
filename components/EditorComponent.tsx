fede9e29c03dc0ae0ce1c0251825580daf45084a
vdevcode <thanhvumaidao1234567@gmail.com>
2024-05-19 18:19:05 +0700

    feat: change language system

--- components/EditorComponent.tsx
+++ components/EditorComponent.tsx
@@ -1,17 +1,23 @@
-import React from 'react'
-import { ModeToggle } from './mode-toggle-btn'
+import React from "react";
+import { ModeToggle } from "./mode-toggle-btn";
+import SelectLanuages from "./SelectLanguages";
 
 const EditorComponent = () => {
   return (
-    <div className='min-h-screen dark:bg-slate-900 rounded-2xl shadow-2xl py-6 px-8'>
-         <div className="flex item-center justify-between">
-            <h2 className="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0">Editor Vcode </h2>
-            <div className="flex">
-                <ModeToggle />
-            </div>
-         </div>
+    <div className="min-h-screen dark:bg-slate-900 rounded-2xl shadow-2xl py-6 px-8">
+      <div className="flex item-center justify-between">
+        <h2 className="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0">
+          Editor Vcode{" "}
+        </h2>
+        <div className="flex items-center gap-4">
+          <div className="w-[230px] flex items-center">
+            <SelectLanuages />
+          </div>
+          <ModeToggle />
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default EditorComponent
+export default EditorComponent;
