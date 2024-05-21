088279439acfea8d3a5e823b834460dd417c5713
vdevcode <thanhvumaidao1234567@gmail.com>
2024-05-21 16:04:10 +0700

    chore: add resizeable shadcn

--- components/EditorComponent.tsx
+++ components/EditorComponent.tsx
@@ -5,7 +5,7 @@ import SelectLanuages from "./SelectLanguages";
 const EditorComponent = () => {
   return (
     <div className="min-h-screen dark:bg-slate-900 rounded-2xl shadow-2xl py-6 px-8">
-      <div className="flex item-center justify-between">
+      <div className="flex item-center justify-between pb-3">
         <h2 className="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0">
           Editor Vcode{" "}
         </h2>
@@ -14,6 +14,12 @@ const EditorComponent = () => {
             <SelectLanuages />
           </div>
           <ModeToggle />
+        </div>
+      </div>
+
+      <div className="bg-slate-400 dark:bg-slate-950 p-3 mt-3">
+        <div className="dark:bg-slate-900">
+            <h2>Editor</h2>
         </div>
       </div>
     </div>
